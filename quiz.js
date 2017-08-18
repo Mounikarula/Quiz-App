ee1c1bb0d9f3901b95d08c1843d3b82a07c03184
mounika <mounika.rgula@gmail.com>
2017-08-18 18:19:57 -0500

    added json data into the app and performed cross browser testing

--- quiz.js
+++ quiz.js
@@ -1,16 +1,15 @@
 (function() {
 	var app = angular.module('funQuiz', []);
 	app.controller('QuizController', ['$scope', '$http', '$sce', function($scope,$http,$sce) {
-		
 
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
 
-
-
-
-
-
-
-
+		$http.get('../quizApp/quiz.json').then(function(quizData) {
+			$scope.myQuestions = quizData.data;
+			$scope.totalQuestions = $scope.myQuestions.length;
+		});
 	}]);
-
-})
+})();
