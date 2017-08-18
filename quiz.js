bfd38d3d1ef328727be7ce0f2aa650c845a17579
mounika <mounika.rgula@gmail.com>
2017-08-18 15:17:02 -0500

    setting up the angularjs and controller in the app

---
+++ quiz.js
@@ -0,0 +1,16 @@
+(function() {
+	var app = angular.module('funQuiz', []);
+	app.controller('QuizController', ['$scope', '$http', '$sce', function($scope,$http,$sce) {
+		
+
+
+
+
+
+
+
+
+
+	}]);
+
+})
